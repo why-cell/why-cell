7740b4cfc581b996a2ed457121faaac82e4e21c3
why-cell <492074921@qq.com>
2020-10-11 14:51:17 +0800

    first commit sdd readme.md

---
+++ app/src/main/java/com/example/a123/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.a123;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.a123.util.DateUtil;
+
+
+public class MainActivity extends Activity { private TextView tv_bbs;
+    private TextView tv_control;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bbs);
+
+        tv_control = findViewById(R.id.tv_control);
+        tv_control.setOnClickListener((View.OnClickListener) this);
+        tv_control.setOnLongClickListener((View.OnLongClickListener) this);
+        tv_bbs = findViewById(R.id.tv_bbs);
+        tv_bbs.setOnClickListener((View.OnClickListener) this);
+        tv_bbs.setOnLongClickListener((View.OnLongClickListener) this);
+        // 设置tv_bbs内部文本的移动方式为滚动形式
+        tv_bbs.setMovementMethod(new ScrollingMovementMethod());
+    }
+
+    private String[] mChatStr = {"你吃饭了吗？", "今天天气真好呀。",
+            "我中奖啦！", "我们去看电影吧", "晚上干什么好呢？",};
+
+
+    public void onClick(View v) {
+        if (v.getId() == R.id.tv_control || v.getId() == R.id.tv_bbs) {
+            // 生成一个0到4之间的随机数
+            int random = (int) (Math.random() * 10) % 5;
+            // 拼接聊天的文本内容
+            String newStr = String.format("%s\n%s %s",
+                    tv_bbs.getText().toString(), DateUtil.getNowTime(), mChatStr[random]);
+            // 设置文本视图tv_bbs的文本内容
+            tv_bbs.setText(newStr);
+        }
+    }
+
+
+    public boolean onLongClick(View v) {
+        if (v.getId() == R.id.tv_control || v.getId() == R.id.tv_bbs) {
+            tv_bbs.setText("");
+        }
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/a123/util/DateUtil.java
@@ -0,0 +1,14 @@
+package com.example.a123.util;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+public class DateUtil {
+    public static String getNowDateTime() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+        return sdf.format(new Date());
+    }
+
+    public static String getNowTime() {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        return sdf.format(new Date());
+    }
+}
